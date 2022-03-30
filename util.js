e92b11baeb0610cec027b021ef9ced4536a2da9c
LiamMcKinney <ljmckinney02@gmail.com>
2022-03-29 19:59:19 -0500

    added move selection system and rudimentary dialog system

--- util.js
+++ util.js
@@ -19,3 +19,31 @@ export function scaleToProp(obj, xPercent, yPercent){
     obj.scaleTo(xScale, yScale);
     });
 }
+
+//from https://www.javascripttutorial.net/javascript-queue/
+export class Queue {
+    constructor() {
+      this.elements = {};
+      this.head = 0;
+      this.tail = 0;
+    }
+    enqueue(element) {
+      this.elements[this.tail] = element;
+      this.tail++;
+    }
+    dequeue() {
+      const item = this.elements[this.head];
+      delete this.elements[this.head];
+      this.head++;
+      return item;
+    }
+    peek() {
+      return this.elements[this.head];
+    }
+    get length() {
+      return this.tail - this.head;
+    }
+    get isEmpty() {
+      return this.length === 0;
+    }
+}
