abc50bbec64c1de65eaea2b29b44a3d0f57a917d
mitchellseifert <mitchellseifert@ku.edu>
2022-03-31 18:35:50 -0500

    added window to printDescription

--- EnemyAI.js
+++ EnemyAI.js
@@ -1,53 +1,15 @@
 export let enemyProto = {
-    hp: 150,
+    hp: 100,
     atk: 10,
     def: 10,
     spAtk: 10,
     spDef: 10,
     name: "defaultEnemy",
     statuses: [],
-    moves: [
-        {
-            name: "Punch",
-            desc: "punch him",
-            pretext: "You punch him",
-            func: ()=>{
-                console.log("Atk1"); 
-                enemy.takeDamage(25);
-                return "ow"}
-        },
-        {
-            name: "Punch",
-            desc: "punch him",
-            pretext: "You punch him",
-            func: ()=>{
-                console.log("Atk2"); 
-                setEnemyHealth(.25);
-                return "ow"}
-        },
-        {
-            name: "Punch",
-            desc: "punch him",
-            pretext: "You punch him",
-            func: ()=>{
-                console.log("Atk3"); 
-                setEnemyHealth(.25);
-                return "ow"}
-        },
-        {
-            name: "Punch",
-            desc: "punch him",
-            pretext: "You punch him",
-            func: ()=>{
-                console.log("Atk4"); 
-                setEnemyHealth(.25);
-                return "ow"}
-        }
-    ],
     die: function(){
-        eventQueue.enqueue(()=>{printDescriptionText(enemyProto.name, "was defeated!")});
-        eventQueue.enqueue(()=>{go("main")});
-        eventQueue.dequeue()();
+        window.eventQueue.enqueue(()=>{printDescriptionText(enemyProto.name, "was defeated!")});
+        window.eventQueue.enqueue(()=>{go("main")});
+        window.eventQueue.dequeue()();
     },
     takeDamage: function(amt){
         this.hp -= amt;
@@ -58,7 +20,7 @@ export let enemyProto = {
         return "Mitchell Just Started to Implement This!";
     },
     enemyMove: function(player){
-
+        
     }
 
 
@@ -71,4 +33,7 @@ barbarian.def = 15
 barbarian.spAtk = 20
 barbarian.atk = 25
 barbarian.def = 25
+barbarian.moves = [
+
+]
 export let wizard = Object.create(enemyProto)
