a167508ed71683d8bd9f65cfebd08118c0d96abf
Matt LaTendresse <matthewlatendresse0803@gmail.com>
2022-04-01 21:23:03 -0500

    added the overWorld, waiting for battleScene to be finidhed to implement other level

--- overWorld.js
+++ overWorld.js
@@ -1,11 +1,10 @@
-
-            // import kaboom lib
-            import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+          // import kaboom lib
+          import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
         
-            import * as util from "./util.js";
+          import * as util from "./util.js";
 
 
-        
+
 
 
 kaboom({background:[60,60,60]});
@@ -14,104 +13,122 @@ loadSprite("wall", "sprites/wall.jpg");
 loadSprite("player1", "sprites/player1.png");
 loadSprite("enemy1", "sprites/enemy1.png");
 loadSprite("door", "sprites/door.png");
+loadSprite("WizardPixel", "sprites/WizardPixel.png");
+
 
 
 
 //we will need to export all these scenes into the main game loop html
 
 scene("overWorld",(levelIndex) =>{
-    onKeyPress("f", (c) => {
-        fullscreen(!isFullscreen())
+onKeyPress("f", (c) => {
+fullscreen(!isFullscreen())
 })
 
 
-    const SPEED = 300;
+const SPEED = 300;
 
-    const levels = [
-        //level one, MARIO vs Barbarian 
-        [
-            "======|=====",
-            "=            =",
-            "=     $      =",
-            "=            =",
-            "=            =",
-            "=            =",
-            "=================",
-        ],
-        //level two, MARIO vs DR. Jonhson
-        [
+const levels = [
+//level one, MARIO vs Barbarian 
+[
+"=======|========",
+"=====     ======",
+"=              =",
+"=           @  =",
+"=              =",
+"=              =",
+"= $            =",
+"================",
+],
+//level two, MARIO vs DR. Jonhson
+[
 
-        ],
-    ]
-
-
-    addLevel(levels[levelIndex],{
-        width:80,
-        height:80,
-        pos:vec2(70,70),
-        "=": () => [
-            sprite("wall"),
-            area(),
-            solid(),
-            scale(0.600),
-
-        ],
-        "|": () => [
-            sprite("door"),
-            area(),
-            solid(),
-            "door",
-            scale(0.24),
-
-        ],
-        "@": () => [
-            sprite("enemy1"),
-            area(),
-            solid(),
-            "enemy1",
-        ],
-        "$": () => [
-            sprite("player1"),
-            area(),
-            solid(),
-            "player1",
-            scale(0.0700),
-        ],
-    })
+],
+]
 
 
-    const player = get("player1")[0]
-    let enemyDead = false;
+addLevel(levels[levelIndex],{
+width:80,
+height:80,
+pos:vec2(70,70),
+"=": () => [
+sprite("wall"),
+area(),
+solid(),
+scale(0.400),
+
+],
+"|": () => [
+sprite("door"),
+area(),
+solid(),
+"door",
+scale(0.16),
+
+],
+"@": () => [
+sprite("WizardPixel"),
+area(),
+solid(),
+"WizardPixel",
+scale(0.4),     
+
+],
+"$": () => [
+sprite("player1"),
+area(),
+solid(),
+"player1",
+scale(0.0800),
+],
+})
 
 
-    player.onCollide("enemy1", (enemy1) => {
-        destroy(enemy1)
-        enemyDead = true;
-        console.log("go to battle scene");
-    })
+const player = get("player1")[0]
+let enemyDead = false;
 
 
-    /*player.onCollide("door", (enemyDead) => {
-		if (enemyDead) {
-			if (levelIndex + 1 < levels.length) {
-				go("overWorld", levelIndex + 1)
-			} 	
-		}
-	})*/
+player.onCollide("WizardPixel", (WizardPixel,door) => {
+destroy(WizardPixel)
+enemyDead = true;
+debug.log("go to battle scene");
+})
 
 
-    const dirs = {
-		"left": LEFT,
-		"right": RIGHT,
-		"up": UP,
-		"down": DOWN,
-	}
+player.onCollide("door", (door) => {
+destroy(door)
+debug.log("Next level...");
+})
 
-    for (const dir in dirs) {
-		onKeyDown(dir, () => {
-			player.move(dirs[dir].scale(SPEED))
-		})
-	}
+/*
+player.onCollide("door", (door) => {
+if (enemyDead) {
+/*
+if (levelIndex + 1 < levels.length) {
+  go("overWorld", levelIndex + 1)
+} 	
+destroy(door)
+}
+})*/
+/*
+const enemyMove = 300
+action('WizardPixel',(s) => {
+s.move(s.dir*enemyMove,0)
+})*/
+
+
+const dirs = {
+"left": LEFT,
+"right": RIGHT,
+"up": UP,
+"down": DOWN,
+}
+
+for (const dir in dirs) {
+onKeyDown(dir, () => {
+player.move(dirs[dir].scale(SPEED))
+})
+}
 
 })
 
