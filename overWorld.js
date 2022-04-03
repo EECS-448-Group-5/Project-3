70c2b5b9b7e40b5dde8793486cf6b2c96bc8291a
Matt LaTendresse <matthewlatendresse0803@gmail.com>
2022-04-03 15:05:19 -0500

    added comments to overWorld.js file

--- overWorld.js
+++ overWorld.js
@@ -1,8 +1,8 @@
-          // import kaboom lib
-          import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+// import kaboom lib
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
         
-          //import * as util from "./util.js";
-          //import "./battleScene.js";
+//import * as util from "./util.js";
+//import "./battleScene.js";
 
 
 
@@ -22,26 +22,26 @@ loadSprite("BarbarianPixel", "sprites/BarbarianPixel.png");
 
 
 //we will need to export all these scenes into the main game loop html
-
+//enter full screen
 scene("overWorld",(levelIndex) =>{
-onKeyPress("f", (c) => {
-fullscreen(!isFullscreen())
+    onKeyPress("f", (c) => {
+    fullscreen(!isFullscreen())
 })
 
-
+//player movement speed
 const SPEED = 300;
 
 const levels = [
 //level one, MARIO vs Barbarian 
 [
-"=======|========",
-"=====     ======",
-"=      !       =",
-"=              =",
-"=              =",
-"=              =",
-"= $            =",
-"================",
+    "=======|========",
+    "=====     ======",
+    "=      !       =",
+    "=              =",
+    "=              =",
+    "=              =",
+    "= $            =",
+    "================",
 ],
 //level two, MARIO vs Wizard
 [
@@ -59,32 +59,32 @@ const levels = [
 
 
 addLevel(levels[levelIndex],{
-width:80,
-height:80,
-pos:vec2(70,70),
+    width:80,
+    height:80,
+    pos:vec2(70,70),
+    //define the chars in the level that associate with png, give them specific attributes
 "=": () => [
-sprite("wall"),
-area(),
-solid(),
-scale(0.400),
+    sprite("wall"),
+    area(),
+    solid(),
+    scale(0.400),
 
 ],
 "|": () => [
-sprite("door"),
-area(),
-solid(),
-"door",
-scale(0.16),
+    sprite("door"),
+    area(),
+    solid(),
+    "door",
+    scale(0.16),
 
 ],
 "@": () => [
-sprite("WizardPixel"),
-area(),
-solid(),
-"WizardPixel",
-scale(0.4),    
+    sprite("WizardPixel"),
+    area(),
+    solid(),
+    "WizardPixel",
+    scale(0.4),    
  
-
 ],
 
 "!": () => [
@@ -96,26 +96,29 @@ scale(0.4),
 ],
  
 "$": () => [
-sprite("player1"),
-area(),
-solid(),
-"player1",
-scale(0.0800),
+    sprite("player1"),
+    area(),
+    solid(),
+    "player1",
+    scale(0.0800),
 ],
 })
 
 
+//get the player object
 const player = get("player1")[0]
 let enemyDead = false;
 
-
+// if player runs into barbarian go to battle scene
 player.onCollide("BarbarianPixel", (BarbarianPixel) => {
-destroy(BarbarianPixel)
-enemyDead = true;
-debug.log("go to battle scene");
-go("battle", 0);
+    destroy(BarbarianPixel)
+    enemyDead = true;
+    debug.log("go to battle scene");
+    go("battle", 0);
 })
 
+//if player runs into wizard go to battle scene 
+//STILL NEEDS IMPLEMENTATION
 player.onCollide("WizardPixel", (WizardPixel) => {
     destroy(WizardPixel)
     enemyDead = true;
@@ -123,13 +126,13 @@ player.onCollide("WizardPixel", (WizardPixel) => {
     //go("battle", 0);
     })
 
-
+//player runs into door go to next level
 player.onCollide("door", (door) => {
-destroy(door)
-debug.log("Next level...");
-if (levelIndex + 1 < levels.length) {
-    go("overWorld", levelIndex + 1)
-  } 
+    destroy(door)
+    debug.log("Next level...");
+    if (levelIndex + 1 < levels.length) {
+        go("overWorld", levelIndex + 1)
+    } 
 })
 
 /*
@@ -148,14 +151,15 @@ action('WizardPixel',(s) => {
 s.move(s.dir*enemyMove,0)
 })*/
 
-
+//movement of player
 const dirs = {
-"left": LEFT,
-"right": RIGHT,
-"up": UP,
-"down": DOWN,
+    "left": LEFT,
+    "right": RIGHT,
+    "up": UP,
+    "down": DOWN,
 }
 
+//listener for player movement
 for (const dir in dirs) {
 onKeyDown(dir, () => {
     player.move(dirs[dir].scale(SPEED))
@@ -170,4 +174,6 @@ onKeyDown(dir, () => {
 
 })
 
+
+//run overWorld loop
 go("overWorld",0)            
