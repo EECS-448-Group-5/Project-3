96cabf2083377f9924a9bc5e87d3553b250136d9
Matt LaTendresse <matthewlatendresse0803@gmail.com>
2022-04-21 19:11:28 -0500

    fixed battle scene

--- battleScene.js
+++ battleScene.js
@@ -18,7 +18,7 @@
     loadSprite("player1", "sprites/player1.png");
     loadSprite("Textbox", "sprites/Textbox.png");
     loadSprite("barbarian", "sprites/barbarianpixel.png");
-    loadSprite("bussinessMan","sprite/businessMan.png");
+    loadSprite("businessMan","sprites/businessMan.png");
 
 scene("battle", (name)=>{
 
