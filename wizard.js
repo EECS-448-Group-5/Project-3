7a52bcbf6cab348d1a1fd1da1058d944f9c98ccb
LiamMcKinney <ljmckinney02@gmail.com>
2022-04-24 15:43:44 -0500

    Added missing features to wizard fight

--- wizard.js
+++ wizard.js
@@ -15,15 +15,17 @@ wizard.setName = function(){
     return((wizard.names[Math.floor(Math.random()*wizard.names.length)]))
 }
 wizard.name = wizard.setName()
+
+wizard.setFlavor("The Wizard chuckles!", "The Wizard smirks at you!", "The wizard begins to float off the ground!")
 wizard.moves = [
     {
         name: "Zap",
         pretext: "The Wizard strikes you with lightning",
         func: (enemy)=>{
             console.log("zap"); 
-            player.takeDamage(30, "magic");
+            player.takeDamage(40, "magic");
             //barbarian.takeDamage(15);
-            return("It was quite shocking, dealing 30 damage")
+            return "It was quite shocking, dealing 40 damage";
         }
     },
     {
@@ -36,13 +38,13 @@ wizard.moves = [
 
             let dmgDealt = pre - player.hp
             enemy.heal(Math.floor(dmgDealt * .75))
-            return("He healed for "+Math.floor(dmgDealt * .75)+" HP!")
+            return "He healed for "+Math.floor(dmgDealt * .75)+" HP!";
         }
     },
 ]
 
 wizard.enemyMove = function(player, move){
-    this.flavorTracker++
+    this.changeFlavor()
         if(wizard.flavorTracker >= 3){
             wizard.flavorTracker = 0; 
         }
@@ -62,5 +64,5 @@ wizard.heal = function(amt){
         this.hp = this.maxHP
     }
 
-    setEnemyHealth(this.hp / this.maxHP)
+    setEnemyHealth(this.hp, this.maxHP)
 }
